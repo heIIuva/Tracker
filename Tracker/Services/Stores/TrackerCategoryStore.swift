2e670a89fd69fa6618c51abe0bce978390035e3b
roman <presenceaura@gmail.com>
2024-11-04 02:04:15 +0300

    implemeted tracker store

---
+++ Tracker/Services/Stores/TrackerCategoryStore.swift
@@ -0,0 +1,14 @@
+//
+//  TrackerCategoryStore.swift
+//  Tracker
+//
+//  Created by big stepper on 04/11/2024.
+//
+
+import Foundation
+import CoreData
+
+
+final class TrackerCategoryStore: NSObject {
+    
+}
---
+++ Tracker/Services/Stores/TrackerStore.swift
@@ -0,0 +1,87 @@
+//
+//  TrackerStore.swift
+//  Tracker
+//
+//  Created by big stepper on 04/11/2024.
+//
+
+
+import UIKit
+import CoreData
+
+
+protocol TrackerStoreProtocol: AnyObject {
+    func fetchTracker(from coreData: TrackerCoreData) -> Tracker?
+    func fetchTrackerCoreData(from tracker: Tracker) -> TrackerCoreData?
+    func fetchTrackerFromCoreDataById(_ id: UUID) -> TrackerCoreData?
+}
+
+
+final class TrackerStore: NSObject {
+    
+    //MARK: - Init
+    
+    private init(
+        context: NSManagedObjectContext,
+        appDelegate: AppDelegate
+    ) {
+        self.context = context
+        self.appDelegate = appDelegate
+    }
+    
+    private convenience override init() {
+        guard
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        else { fatalError("Could not initialize TrackerStore") }
+        
+        self.init(
+            context: appDelegate.persistentContainer.viewContext,
+            appDelegate: appDelegate
+        )
+    }
+    
+    static let shared = TrackerStore()
+    
+    //MARK: - Properties
+    
+    private let uiColorMarshalling = UIColorMarshalling()
+    private let context: NSManagedObjectContext
+    private let appDelegate: AppDelegate
+}
+
+//MARK: - TrackerStoreProtocol
+extension TrackerStore: TrackerStoreProtocol {
+    
+    func fetchTracker(from coreData: TrackerCoreData) -> Tracker? {
+        guard
+            let id = coreData.id,
+            let name = coreData.name,
+            let colorHex = coreData.color,
+            let emoji = coreData.emoji,
+            let timeTable = coreData.timeTable as? [Weekday]
+        else { return nil }
+        
+        return Tracker(
+            id: id,
+            name: name,
+            color: uiColorMarshalling.color(from: colorHex),
+            emoji: emoji,
+            timeTable: timeTable
+        )
+    }
+    
+    func fetchTrackerCoreData(from tracker: Tracker) -> TrackerCoreData? {
+        let trackerCoreData = TrackerCoreData(context: context)
+        trackerCoreData.id = tracker.id
+        trackerCoreData.name = tracker.name
+        trackerCoreData.color = uiColorMarshalling.hexString(from: tracker.color)
+        trackerCoreData.timeTable = tracker.timeTable as NSObject
+        return trackerCoreData
+    }
+    
+    func fetchTrackerFromCoreDataById(_ id: UUID) -> TrackerCoreData? {
+        let request: NSFetchRequest<TrackerCoreData> = TrackerCoreData.fetchRequest()
+        request.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+        return try? context.fetch(request).first
+    }
+}
